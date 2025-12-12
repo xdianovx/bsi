becedb5b78c3b37abd600f052fbc2a21e8b6a9bf
xdianovx <dianov.js@gmail.com>
2025-12-12 17:08:35 +0300

    Header mob

--- header.php
+++ header.php
@@ -120,7 +120,7 @@
           <div class="header-old-btns">
             <a href="https://www.bsigroup.ru/"
                target="_blank"
-               class="button-login header__button-login">Старый сайт</a>
+               class="button-login header__button-login button-old-site">Старый сайт</a>
             <a href="https://online.bsigroup.ru/cl_refer"
                class="button-login header__button-login">
 
@@ -144,32 +144,21 @@
               </svg>
               <span>Личный кабинет</span>
             </a>
+
+            <div class="burger">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
 
         </div>
       </div>
 
 
-      <a href="#"
-         class="button-login header__button-login --mob">
-        <svg width="16"
-             height="17"
-             viewBox="0 0 16 17"
-             fill="#EE3145"
-             xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8.00043 10.3303C6.45389 10.3294 4.94413 10.8019 3.67393 11.6841C2.40373 12.5664 1.4339 13.8162 0.894714 15.2657C0.702715 15.7783 1.11243 16.2857 1.65929 16.2857H14.3381C14.8867 16.2857 15.2947 15.7783 15.1044 15.2657C14.5652 13.8162 13.5954 12.5664 12.3252 11.6841C11.055 10.8019 9.54696 10.3294 8.00043 10.3303ZM11.7856 4.6474C11.7856 5.65242 11.3863 6.61627 10.6757 7.32693C9.96501 8.03759 9.00116 8.43683 7.99614 8.43683C6.99112 8.43683 6.02727 8.03759 5.31661 7.32693C4.60596 6.61627 4.20671 5.65242 4.20671 4.6474C4.20671 3.64238 4.60596 2.67852 5.31661 1.96787C6.02727 1.25721 6.99112 0.857971 7.99614 0.857971C9.00116 0.857971 9.96501 1.25721 10.6757 1.96787C11.3863 2.67852 11.7856 3.64238 11.7856 4.6474Z">
-          </path>
-        </svg>
-      </a>
 
-      <div class="burger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
+
+
     </div>
   </header>
 
