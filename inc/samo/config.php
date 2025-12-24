04631a9f39852725c57ceaf6fe0e345cf085e8b4
xdianovx <dianov.js@gmail.com>
2025-12-24 15:06:26 +0300

    asdasd

--- inc/samo/config.php
+++ inc/samo/config.php
@@ -3,8 +3,8 @@
 function samo_config(): array
 {
   return [
-    'base_url' => 'https://online.bsigroup.ru/export/default.php',
-    'token' => 'ddcb768f480a4d769bc960c76ac29528',
+    'base_url' => SAMO_API_URL,
+    'token' => SAMO_OAUTH_TOKEN,
     'samo_action' => defined('SAMO_ACTION') ? SAMO_ACTION : 'api',
     'version' => defined('SAMO_API_VERSION') ? SAMO_API_VERSION : '1.0',
     'type' => defined('SAMO_API_TYPE') ? SAMO_API_TYPE : 'json',
