c4a5a6a6c167944ffa4ac8b906383f2738f9f338
xdianovx <dianov.js@gmail.com>
2025-12-27 22:38:57 +0300

    asd

--- template-parts/hotels/card.php
+++ template-parts/hotels/card.php
@@ -127,6 +127,27 @@ if (function_exists('get_field')) {
   </div>
 
   <div class="hotel-card__body">
+
+
+    <div class="hotel-card__title-wrap">
+      <?php if ($rating > 0): ?>
+        <div class="hotel-card__rating">
+          <?php for ($i = 1; $i <= 5; $i++): ?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+              fill="<?php echo $i <= $rating ? 'currentColor' : 'none'; ?>" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round"
+              class="lucide lucide-star-icon lucide-star <?php echo $i <= $rating ? 'filled' : ''; ?>">
+              <path
+                d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z">
+              </path>
+            </svg>
+          <?php endfor; ?>
+        </div>
+      <?php endif; ?>
+      <h3 class="hotel-card__title"><?php echo esc_html($hotel_title); ?></h3>
+
+    </div>
+
     <div class="hotel-card__location">
       <?php if ($hotel_flag): ?>
         <div class="hotel-card__flag">
@@ -142,24 +163,6 @@ if (function_exists('get_field')) {
           <span class="hotel-card__resort"><?php echo esc_html($resort_title); ?></span>
         <?php endif; ?>
       </div>
-    </div>
-
-    <div class="hotel-card__title-wrap">
-      <h3 class="hotel-card__title"><?php echo esc_html($hotel_title); ?></h3>
-      <?php if ($rating > 0): ?>
-        <div class="hotel-card__rating">
-          <?php for ($i = 1; $i <= 5; $i++): ?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-              fill="<?php echo $i <= $rating ? 'currentColor' : 'none'; ?>" stroke="currentColor" stroke-width="2"
-              stroke-linecap="round" stroke-linejoin="round"
-              class="lucide lucide-star-icon lucide-star <?php echo $i <= $rating ? 'filled' : ''; ?>">
-              <path
-                d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z">
-              </path>
-            </svg>
-          <?php endfor; ?>
-        </div>
-      <?php endif; ?>
     </div>
 
     <div class="hotel-card__anemeties">
