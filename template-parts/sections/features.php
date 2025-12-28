6a90e13b20df384bbb880de0932482e53aa77bcb
xdianovx <dianov.js@gmail.com>
2025-12-28 23:19:08 +0300

    asdasd

--- template-parts/sections/features.php
+++ template-parts/sections/features.php
@@ -5,22 +5,34 @@
       <div class="features-item --main">
         <div class="features-item__title">> 35 лет</div>
         <div class="features-item__subtitle">в туризме</div>
+
+        <img class="features-item__img" src="<?php echo get_template_directory_uri(); ?>/img/features/1.png"
+          alt="35 лет в туризме">
       </div>
 
       <div class="features-col">
         <div class="features-item">
           <div class="features-item__title">2 000 000</div>
           <div class="features-item__subtitle">клиентов</div>
+
+          <img class="features-item__img" src="<?php echo get_template_directory_uri(); ?>/img/features/2.png"
+            alt="2 000 000 клиентов">
         </div>
 
         <div class="features-row">
           <div class="features-item">
             <div class="features-item__title">3 000</div>
             <div class="features-item__subtitle">Турагенств</div>
+
+            <img class="features-item__img" src="<?php echo get_template_directory_uri(); ?>/img/features/3.png"
+              alt="3 000 турагенств">
           </div>
           <div class="features-item">
-            <div class="features-item__title">>30</div>
+            <div class="features-item__title">> 30</div>
             <div class="features-item__subtitle">Стран и направлений</div>
+
+            <img class="features-item__img" src="<?php echo get_template_directory_uri(); ?>/img/features/4.png"
+              alt="30 стран и направлений">
           </div>
         </div>
       </div>
