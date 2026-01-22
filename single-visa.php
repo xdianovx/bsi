6d4d68a849a9c89ca91861804e4093338ca214c1
xdianovx <dianov.js@gmail.com>
2026-01-22 16:26:32 +0300

    фыв

--- single-visa.php
+++ single-visa.php
@@ -37,18 +37,6 @@ get_header();
           ?>
           <div class="visa-page__info-section">
             <div class="visa-info-item__wrap">
-
-
-              <?php if ($consular_fee): ?>
-                <div class="visa-info-item">
-                  <div class="visa-info-item__title">
-                    <p class="visa-info-item__key">Консульский и сервисный сборы</p>
-                  </div>
-                  <p class="visa-info-item__value"><?= esc_html($consular_fee); ?></p>
-                </div>
-              <?php endif; ?>
-
-
               <?php if ($validity_period): ?>
                 <div class="visa-info-item">
                   <div class="visa-info-item__title">
@@ -60,6 +48,26 @@ get_header();
                 </div>
               <?php endif; ?>
 
+              <?php if ($consular_fee): ?>
+                <div class="visa-info-item">
+                  <div class="visa-info-item__title">
+                    <p class="visa-info-item__key">Консульский и сервисный сборы</p>
+                  </div>
+                  <p class="visa-info-item__value"><?= esc_html($consular_fee); ?></p>
+                </div>
+              <?php endif; ?>
+
+
+              <?php if ($processing_time): ?>
+                <div class="visa-info-item">
+                  <div class="visa-info-item__title">
+                    <p class="visa-info-item__key">Срок оформления</p>
+                  </div>
+                  <p class="visa-info-item__value">
+                    <?= esc_html($processing_time); ?>
+                  </p>
+                </div>
+              <?php endif; ?>
 
               <?php if ($support_fee): ?>
                 <div class="visa-info-item">
@@ -72,14 +80,7 @@ get_header();
                 </div>
               <?php endif; ?>
 
-              <?php if ($processing_time): ?>
-                <div class="visa-info-item">
-                  <div class="visa-info-item__title">
-                    <p class="visa-info-item__key">Срок оформления</p>
-                  </div>
-                  <p class="visa-info-item__value"><?= esc_html($processing_time); ?></p>
-                </div>
-              <?php endif; ?>
+
 
 
 
