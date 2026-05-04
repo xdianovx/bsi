7cff3e5d22256ac00727fb74ae00ae4185ce8cf6
xdianovx <dianov.js@gmail.com>
2026-05-04 16:19:10 +0300

    выа

--- footer.php
+++ footer.php
@@ -173,6 +173,22 @@
 <?= get_template_part('template-parts/maintenance-modal') ?>
 
 
+<!-- Yandex.Metrika counter — всегда (баннер cookie фиксирует выбор пользователя отдельно) -->
+<script type="text/javascript">
+  (function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+    m[i].l = 1 * new Date();
+    for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=108341897', 'ym');
+
+  ym(108341897, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce: true, trackLinks: true });
+</script>
+<noscript>
+  <div><img src="https://mc.yandex.ru/watch/108341897" style="position:absolute; left:-9999px;" alt="" /></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php wp_footer(); ?>
 <script src="https://api-maps.yandex.ru/v3/?apikey=e2acacad-47ea-4ef2-8273-61a3a5f50c5b&lang=ru_RU"></script>
 
