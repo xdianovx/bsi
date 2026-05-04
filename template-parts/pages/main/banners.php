99c016dcba78936ac0a59a1f3756a06185b53db5
xdianovx <dianov.js@gmail.com>
2026-05-04 17:17:38 +0300

    фыв

--- template-parts/pages/main/banners.php
+++ template-parts/pages/main/banners.php
@@ -8,27 +8,30 @@ $banners = get_field('banners', get_the_ID());
       <div class="swiper-wrapper">
 
 
-        <?php foreach ($banners as $banner): ?>
+        <?php foreach ($banners as $i => $banner): ?>
+          <?php
+          $banner_alt   = esc_attr($banner['title'] ?? 'Баннер BSI Group');
+          $is_first     = ($i === 0);
+          $loading      = $is_first ? 'eager' : 'lazy';
+          $fetchpriority = $is_first ? ' fetchpriority="high"' : '';
+          ?>
           <div class="swiper-slide">
-            <?php $banner_alt = esc_attr($banner['title'] ?? 'Баннер BSI Group'); ?>
             <?php if (!empty($banner['url'])): ?>
               <a href="<?= esc_url($banner['url']); ?>" target="_blank" rel="noopener noreferrer" class="main-banner__slide">
                 <?php if (!empty($banner['mobilnyj_banner'])): ?>
-                  <img class="main-banner__slide_image main-banner__slide_image--desktop" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" />
-                  <img class="main-banner__slide_image main-banner__slide_image--mobile"
-                    src="<?= esc_url($banner['mobilnyj_banner']); ?>" alt="<?= $banner_alt; ?>" />
+                  <img class="main-banner__slide_image main-banner__slide_image--desktop" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
+                  <img class="main-banner__slide_image main-banner__slide_image--mobile" src="<?= esc_url($banner['mobilnyj_banner']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
                 <?php else: ?>
-                  <img class="main-banner__slide_image" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" />
+                  <img class="main-banner__slide_image" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
                 <?php endif; ?>
               </a>
             <?php else: ?>
               <div class="main-banner__slide">
                 <?php if (!empty($banner['mobilnyj_banner'])): ?>
-                  <img class="main-banner__slide_image main-banner__slide_image--desktop" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" />
-                  <img class="main-banner__slide_image main-banner__slide_image--mobile"
-                    src="<?= esc_url($banner['mobilnyj_banner']); ?>" alt="<?= $banner_alt; ?>" />
+                  <img class="main-banner__slide_image main-banner__slide_image--desktop" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
+                  <img class="main-banner__slide_image main-banner__slide_image--mobile" src="<?= esc_url($banner['mobilnyj_banner']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
                 <?php else: ?>
-                  <img class="main-banner__slide_image" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" />
+                  <img class="main-banner__slide_image" src="<?= esc_url($banner['img']); ?>" alt="<?= $banner_alt; ?>" loading="<?= $loading; ?>"<?= $fetchpriority; ?> decoding="async" />
                 <?php endif; ?>
               </div>
             <?php endif; ?>
